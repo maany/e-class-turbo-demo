73aa197807fddb96b18eac4c36baf6cf8acc943c
jatingodnani <godnanijatin@gmail.com>
2024-11-13 19:53:09 +0530

    pnpm

--- apps/web/app/[locale]/table/page.tsx
+++ apps/web/app/[locale]/table/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <div className='w-full flex items-center justify-center'>
       {/* <h1>AG Grid Table</h1> */}
+      <h1>table</h1>
       <AgGridTable />
     </div>
   );
